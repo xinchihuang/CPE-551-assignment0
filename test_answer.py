41177ed1f6141e3b8a8f10b4d94923250fde05ad
xinchihuang <xhuang60@stevens.edu>
2022-08-27 19:29:29 -0400

    Update test_answer.py

--- test_answer.py
+++ test_answer.py
@@ -1,8 +1,8 @@
 import answer
 
 def test_add():
-    assert (add(1, 2) == 3)
-    assert (add(1, 1) == 2)
-    assert (add(100, 200) == 300)
-    assert (add(0, 1) == 1)
-    assert (add(-1, 1) == 0)
+    assert (answer.add(1, 2) == 3)
+    assert (answer.add(1, 1) == 2)
+    assert (answer.add(100, 200) == 300)
+    assert (answer.add(0, 1) == 1)
+    assert (answer.add(-1, 1) == 0)
